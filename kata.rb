3b4158ddc7cc451b1e61ec1bd28336b714c34b1d
Christopher Harrop <bannastre@gmail.com>
2017-04-27 11:31:52 +0100

    Create kata.rb
    This is the solution to be worked on with in pairing

---
+++ kata.rb
@@ -0,0 +1,12 @@
+def pick_peaks(arr)
+  pos = []
+  peaks = []
+  
+  # find the peaks in the arary
+  
+  # add the peak numbers to the array 'peaks'
+  
+  # find the position of the peak numbers (by index?)
+  
+  # add the position index to the 'pos' array
+end
